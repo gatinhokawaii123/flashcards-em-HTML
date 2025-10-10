76104c21069c3af43c8bb8816eb6a9db78a3e186
gatinhokawaii123 <talita.alves.machado@escola.pr.gov.br>
2025-10-10 08:02:30 -0300

    .......

---
+++ assets/perguntas.js
@@ -0,0 +1,15 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de progração'
+)
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris',
+)
+criaCartao(
+    'Progração',
+    'O que é uma função',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
